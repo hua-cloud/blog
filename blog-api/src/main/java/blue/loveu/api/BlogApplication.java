92b04318bfa17e70659a53bc6e1dddfabb581dda
nacho <nachoneco6@gmail.com>
2025-08-06 22:56:17 +0800

    调整pom

--- blog-api/src/main/java/blue/loveu/api/BlogApplication.java
+++ blog-api/src/main/java/blue/loveu/api/BlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApiApplication {
+public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogApiApplication.class, args);
+        SpringApplication.run(BlogApplication.class, args);
+        System.out.println("项目启动成功");
     }
-
 }
